1ca56a20ee49be42b142e837719fd0000067394c
Thuc Phan <ttp293@vt.edu>
2019-02-17 01:45:34 -0500

    Initial commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public float rotationSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.RotateAround(Vector3.up, rotationSpeed * Time.deltaTime);
+		
+	}
+}
